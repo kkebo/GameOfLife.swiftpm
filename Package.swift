87c71c5d6dddd7c551ff29f1e2e3501d93d2560b
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-06-10 00:05:59 +0900

    feat: enable Embedded Swift

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         .target(
             name: "GameOfLife",
             swiftSettings: [
+                .enableExperimentalFeature("Embedded"),
                 .enableExperimentalFeature("StrictConcurrency"),
                 .enableUpcomingFeature("BareSlashRegexLiterals"),
                 .enableUpcomingFeature("ExistentialAny"),
